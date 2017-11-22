bb2f30f2bba9802af2a2f8719987b9f89962b4d3
davigonz <dgonzalez@solidgear.es>
2017-11-22 10:54:19 +0100

    First commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,31 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { CameraOptions, Camera } from '@ionic-native/camera';
 
 @Component({
-  selector: 'page-home',
-  templateUrl: 'home.html'
+    selector: 'page-home',
+    templateUrl: 'home.html'
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, private camera : Camera) {
 
-  }
+    }
 
+    private options : CameraOptions = {
+        quality: 100,
+        destinationType: this.camera.DestinationType.DATA_URL,
+        encodingType: this.camera.EncodingType.JPEG,
+        mediaType: this.camera.MediaType.PICTURE
+    }
+
+    private image;
+
+    hacerFoto() {
+        this.camera.getPicture(this.options).then((imageData) => {
+            this.image = 'data:image/jpeg;base64,' + imageData;
+        }, (err) => {
+
+        });
+    }
 }
